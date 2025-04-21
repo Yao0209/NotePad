6c20c26c757faca4652096beee537fb7d181dd04
yu-yao <ff20060209@gmail.com>
2025-04-21 15:08:39 +0800

    新增基本設定
    增加word

--- Form1.cs
+++ Form1.cs
@@ -23,7 +23,7 @@ namespace NotePad
             // 設置對話方塊標題
             openFileDialog1.Title = "選擇檔案";
             // 設置對話方塊篩選器，限制使用者只能選擇特定類型的檔案
-            openFileDialog1.Filter = "Word檔案 (*.docx)|文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*";
+            openFileDialog1.Filter = "Word檔案 (*.docx)|*.docx|文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*";
             // 如果希望預設開啟的檔案類型是文字檔案，可以這樣設置
             openFileDialog1.FilterIndex = 1;
             // 如果希望對話方塊在開啟時顯示的初始目錄，可以設置 InitialDirectory
