ad52691eb34a5ccd0330c4d90bf68572bc89156c
yu-yao <ff20060209@gmail.com>
2025-04-21 14:18:08 +0800

    進行介面基本設計
    加入Button按鍵、控制項：豐富文件輸入文字框 (RichTextBox)、控制項：對話方塊（Dialog）
    Button按鍵更改text
    將按鍵、控制項的(name)更改名字:
    開啟檔案按鍵：btnOpen
    存檔按鍵：btnSave
    文字編輯區（RichTextBox）：rtbText
    對話方塊（Dialog）：openFileDialog1、saveFileDialog1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(12, 116);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(1236, 668);
+            this.rtbText.TabIndex = 0;
+            this.rtbText.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btnOpen.Font = new System.Drawing.Font("標楷體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnOpen.Location = new System.Drawing.Point(123, 22);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(158, 70);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "開啟檔案";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btnSave.Font = new System.Drawing.Font("標楷體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSave.Location = new System.Drawing.Point(381, 22);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(158, 70);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "存檔";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 468);
+            this.ClientSize = new System.Drawing.Size(1260, 796);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.rtbText);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +84,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
