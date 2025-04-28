44e3661d0f98cd378bae4e2aad913216212224ff
yu-yao <ff20060209@gmail.com>
2025-04-28 15:13:29 +0800

    新增功能:記事本（復原上一步）
    新增:ctrl+z(回上一步)的功能
    第一部分：介面設定
    步驟
    1.新增一個新的控制項「清單方塊（ListBox）」
    2.為清單方塊與按鍵進行命名，清單方塊：listUndo、復原上一步按鍵：btnUndo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,14 +33,16 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnSave = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listUndo = new System.Windows.Forms.ListBox();
+            this.btnUndo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rtbText
             // 
             this.rtbText.BackColor = System.Drawing.SystemColors.Window;
-            this.rtbText.Location = new System.Drawing.Point(24, 122);
+            this.rtbText.Location = new System.Drawing.Point(6, 106);
             this.rtbText.Name = "rtbText";
-            this.rtbText.Size = new System.Drawing.Size(1224, 662);
+            this.rtbText.Size = new System.Drawing.Size(945, 678);
             this.rtbText.TabIndex = 0;
             this.rtbText.Text = "";
             // 
@@ -48,7 +50,7 @@
             // 
             this.btnOpen.DialogResult = System.Windows.Forms.DialogResult.Retry;
             this.btnOpen.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnOpen.Location = new System.Drawing.Point(105, 12);
+            this.btnOpen.Location = new System.Drawing.Point(74, 12);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(212, 88);
             this.btnOpen.TabIndex = 1;
@@ -66,7 +68,7 @@
             this.btnSave.DialogResult = System.Windows.Forms.DialogResult.Retry;
             this.btnSave.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btnSave.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnSave.Location = new System.Drawing.Point(495, 12);
+            this.btnSave.Location = new System.Drawing.Point(371, 12);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(214, 88);
             this.btnSave.TabIndex = 2;
@@ -74,11 +76,33 @@
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // listUndo
+            // 
+            this.listUndo.FormattingEnabled = true;
+            this.listUndo.ItemHeight = 24;
+            this.listUndo.Location = new System.Drawing.Point(971, 106);
+            this.listUndo.Name = "listUndo";
+            this.listUndo.Size = new System.Drawing.Size(301, 676);
+            this.listUndo.TabIndex = 3;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btnUndo.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnUndo.Location = new System.Drawing.Point(656, 12);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(222, 88);
+            this.btnUndo.TabIndex = 4;
+            this.btnUndo.Text = "復原上一步";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1260, 796);
+            this.ClientSize = new System.Drawing.Size(1274, 796);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.listUndo);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.rtbText);
@@ -95,6 +119,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox listUndo;
+        private System.Windows.Forms.Button btnUndo;
     }
 }
 
